7bcf1c1edd24938bd01e56700ee52e0251d7b9a8
liahoney <toridoc@gmail.com>
2023-01-26 19:08:00 +0900

    calendar making

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import dayjs from 'dayjs';
+import { useEffect } from 'react';
+import { FlatList, StyleSheet, Text } from 'react-native';
+import { getCalendarColumns } from './src/util';
+import { View } from 'react-native';
+
+
+const columnSize = 30;
+
+const renderItem = ({ item : date}) => {
+  const dateText = dayjs(date).get('date')
+  const day = dayjs(date).get('day')
+  const color = day === 0 ? "#e67639" : day === 6 ? "#5872d1" : "#2b2b2b";
+  return (
+    <View style={{width: columnSize, height: columnSize, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{color}}>{dateText}</Text>
+    </View>
+    
+  )
+}
 
 export default function App() {
+  console.log('hi')
+  const now = dayjs();
+  const columns = getCalendarColumns(now);
+
+  useEffect(() => { 
+    console.log('columns', columns);
+  })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+     <FlatList
+      data={columns}
+      numColumns={7} 
+      renderItem={renderItem}
+      />
+    </SafeAreaView>
   );
 }
 
