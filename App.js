184771cdb602eaca672a16b5e349b184fd422eac
liahoney <toridoc@gmail.com>
2023-01-27 16:47:02 +0900

    calendar complete

--- App.js
+++ App.js
@@ -1,61 +1,161 @@
-import { SafeAreaView } from 'react-native';
-import dayjs from 'dayjs';
-import { useEffect } from 'react';
-import { FlatList, StyleSheet, Text } from 'react-native';
-import { getCalendarColumns } from './src/util';
-import { View } from 'react-native';
-import { getDayText, getDayColor } from './src/util';
-
+import { SafeAreaView, TouchableOpacity, ArrowButton } from "react-native";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { FlatList, StyleSheet, Text } from "react-native";
+import { getCalendarColumns } from "./src/util";
+import { View } from "react-native";
+import { getDayText, getDayColor } from "./src/util";
+import Margin from "./src/Margin";
+import { SimpleLineIcons } from "@expo/vector-icons";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 const columnSize = 30;
 
-const Column = ({text, color, opacity }) => {
+const Column = ({ text, color, opacity, disabled, onPress, isSelected }) => {
   return (
-    <View style={{width: columnSize, height: columnSize, justifyContent: "center", alignItems: "center" }}>
-        <Text style={{color, opacity}}>{text}</Text>
-      </View>
-  )
-}
+    <TouchableOpacity
+      disabled={disabled}
+      onPress={onPress}
+      style={{
+        width: columnSize,
+        height: columnSize,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: isSelected?  "#c2c2c2" : "transparent",
+        borderRadius: columnSize / 2,
+      }}
+    >
+      <Text style={{ color, opacity }}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
 
 export default function App() {
- 
   const now = dayjs();
-  const columns = getCalendarColumns(now);
+  const [selectedDate, setSelectedDate] = useState(now);
+  const columns = getCalendarColumns(selectedDate);
 
-  const ListHeaderComponent = () => (
-    <View style={{flexDirection: 'row'}}>
-       {/* 일~토 */}
-    {[0,1,2,3,4,5,6].map(day => {
-      const dayText = getDayText(day);
-      const color = getDayColor(day)
-      return (
-        <Column text={dayText} color={color} opacity={1}/>
-      )
-    })}
-    </View>
+
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
    
-  )
-  const renderItem = ({ item : date}) => {
-    const dateText = dayjs(date).get('date')
-    const day = dayjs(date).get('day')
-    const color = day === 0 ? "#e67639" : day === 6 ? "#5872d1" : "#2b2b2b";
-    const isCurrentMonth = dayjs(date).isSame(now, 'month');
-    return (
-      <Column text={dateText} color={color} opacity={isCurrentMonth ? 1: 0.4}/>
-    
-    )
+    setSelectedDate(dayjs(date))
+    hideDatePicker();
+  };
+
+  const onPressLeftArrow = () => {
+    const newSelectedDate = dayjs(selectedDate).subtract(1, 'month')
+    setSelectedDate(newSelectedDate)
   }
 
-  useEffect(() => { 
+  const onPressRightArrow = () => {
+    const newSelectedDate = dayjs(selectedDate).add(1, 'month')
+    setSelectedDate(newSelectedDate)
+  }
+
+  const ArrowButton = ({onPress, iconName}) => {
+    return (
+      <TouchableOpacity onPress={onPress} style={{paddingHorizontal: 20, paddingVertical: 15}}>
+            <SimpleLineIcons name={iconName} size={15} color="#404040" />
+          </TouchableOpacity>
+    )
+  }
+  const ListHeaderComponent = () => {
+    const currentDateText = dayjs(selectedDate).format("YYYY.MM.DD.");
+    return (
+      <View>
+        
+        {/* YYYY.MM.DD */}
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <ArrowButton iconName="arrow-left" onPress={onPressLeftArrow}/>
+
+          <TouchableOpacity onPress={showDatePicker}>
+            <Text style={{ fontSize: 20, color: "#404040" }}>
+              {currentDateText}
+            </Text>
+          </TouchableOpacity>
+
+          <ArrowButton iconName="arrow-right" onPress={onPressRightArrow}/>
+
+        </View>
+        
+
+        <View style={{ flexDirection: "row" }}>
+          {/* 일~토 */}
+          {[0, 1, 2, 3, 4, 5, 6].map((day) => {
+            const dayText = getDayText(day);
+            const color = getDayColor(day);
+            return (
+              <Column
+                key={`day-${day}`}
+                text={dayText}
+                color={color}
+                opacity={1}
+                disabled={true}
+              />
+            );
+          })}
+        </View>
+      </View>
+    );
+  };
+
+  const renderItem = ({ item: date }) => {
+    const dateText = dayjs(date).get("date");
+    const day = dayjs(date).get("day");
+    const color = day === 0 ? "#e67639" : day === 6 ? "#5872d1" : "#2b2b2b";
+    const isCurrentMonth = dayjs(date).isSame(selectedDate, "month");
+    const onPress = () => {
+      setSelectedDate(date)
+    }
+    const isSelected = dayjs(date).isSame(selectedDate, 'date')
+    return (
+      <Column
+        text={dateText}
+        color={color}
+        opacity={isCurrentMonth ? 1 : 0.4}
+        onPress={onPress}
+        isSelected={isSelected}
+      />
+    );
+  };
+
+  useEffect(() => {
     // console.log('columns', columns);
-  })
+  });
+
+  useEffect(() => {
+    console.log('change selectedDate?', dayjs(selectedDate).format("YYYY.MM.DD"))
+  },[selectedDate])
   return (
     <SafeAreaView style={styles.container}>
-     <FlatList
-      data={columns}
-      numColumns={7} 
-      renderItem={renderItem}
-      ListHeaderComponent={ListHeaderComponent}
+      <FlatList
+        data={columns}
+        numColumns={7}
+        renderItem={renderItem}
+        ListHeaderComponent={ListHeaderComponent}
+        keyExtractor={(_, index) => `column-${index}`}
+      />
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
       />
     </SafeAreaView>
   );
@@ -64,8 +164,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
